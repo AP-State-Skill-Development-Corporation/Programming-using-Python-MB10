3a6c9df899eb7d35faaa7de40a733f2da21a5a61
GAlekhya123 <51401978+GAlekhya123@users.noreply.github.com>
2020-12-17 19:33:56 +0530

    Added day 10 discussed content

---
+++ Day10_17Dec2020/mymodule.py
@@ -0,0 +1,10 @@
+def evenodd(n):
+    if n%2==0:
+        print("even")
+    else:
+        print("odd")
+def add(a,b):
+    c = a+b
+    print(c)
+def hello(name):
+    print("Hello"+" "+name)
---
+++ Day10_17Dec2020/mypackage/module.py
@@ -0,0 +1,15 @@
+def isprime(n):
+    c=0
+    for i in range(1,n+1):
+        if n%i==0:
+            c+=1
+    if c==2:
+        print("prime number")
+    else:
+        print("not a prime number")
+
+def factorial(n):
+    f=1
+    for i in range(1,n+1):
+        f*=i
+    print(f)
